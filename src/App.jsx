2216f3990bd67989b1eaa6c4aa1e908bfd3c9ac9
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-18 20:11:26 -0300

    trabajando en el componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import ProductDetail from "./components/ProductDetail";
+
 
 function App() {
-  return <></>;
+  return <>
+  <h1>Detalle del producto</h1>
+  <div>
+    <ProductDetail/>
+  </div>
+  </>;
 }
 
 export default App;
